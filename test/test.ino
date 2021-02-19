8e62ef1283a6037bc6d5449a41d09a80c4d485fd
Matuesz <bieganski.m@wp.pl>
2021-02-19 18:55:10 +0100

    added deep sleep code, now port BLE to it

--- test/test.ino
+++ test/test.ino
@@ -1,37 +1,21 @@
-#include <WiFi.h>
-#include "private.h"
 
-char* API_KEY = "a4c7378a-5798-40f6-a9c1-6a0fae8c4da4";
-char* MACZKA_ID = "1474";
-// Use this one to obtain "Maczka" bus stop ID
-// String URL = "api.um.warszawa.pl/api/action/dbtimetable_get/?id=b27f4c17-5c50-4a5b-89dd-236b282bc499&name=Maczka&apikey="
+#include <esp_sleep.h>
 
-char* URL = "https://api.um.warszawa.pl/api/action/dbtimetable_get?id=e923fa0e-d96c-43f9-ae6e-60518c9f3238&busstopId=%s&busstopNr=01&line=245&apikey=%s";
+#define _DEEP_SLEEP_TIME_SEC 3
 
-char url_placeholder[300];
+#define DEEP_SLEEP_TIME (_DEEP_SLEEP_TIME_SEC * 1000000L)
 
-void setup_wifi() {
-		WiFi.begin(SSID, PASSWORD);
-
-		while (WiFi.status() != WL_CONNECTED) {
-			delay(500);
-			Serial.print(".");
-		}
-}
 
 void setup() {
-	Serial.begin(115200);
-	delay(10);
-	setup_wifi();
+    Serial.println("REBOOT!");
+    // delay(100); // it doesn't print without that delay, how come? two cores used or what
 
-	Serial.println("");
-	Serial.println("WiFi connected");
-	Serial.println("IP address: ");
-	Serial.println(WiFi.localIP());
+    esp_sleep_enable_timer_wakeup(DEEP_SLEEP_TIME);
 
-	sprintf(url_placeholder, URL, MACZKA_ID, APIKEY);
-	Serial.println(url_placeholder);
+    // go to sleep, after wakeup 'setup' will be called
+    esp_deep_sleep_start();
 }
 
 void loop() {
+    Serial.print("ERROR: I shouldn't be in loop anytime! Deep sleep wakeup implies reboot!");
 }
